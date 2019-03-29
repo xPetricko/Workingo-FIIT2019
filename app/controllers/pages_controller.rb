0e63180dc0fcad8a94787488afb81c6df5eaa8b6
Andrej Petričko <32256397+xPetricko@users.noreply.github.com>
2019-03-29 23:35:57 +0100

    piatok
    piatok

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+  def homepage
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  root 'application#hello'
+
+  root 'pages#homepage'
+
+  get  '/help',    to: 'pages#help'
+  get  '/about',   to: 'pages#about'
+  get  '/contact', to: 'pages#contact'
+
+
 end
