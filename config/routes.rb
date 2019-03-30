e2ef6b2f2a735fa4a455360d9ec4fb7a1ef25285
Andrej Petričko <32256397+xPetricko@users.noreply.github.com>
2019-03-30 14:02:32 +0100

    sobota  14.00

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
   root 'pages#homepage'
 
   get  '/help',    to: 'pages#help'
   get  '/about',   to: 'pages#about'
   get  '/contact', to: 'pages#contact'
-
+  get  '/signup',  to: 'users#new'
 
 end
