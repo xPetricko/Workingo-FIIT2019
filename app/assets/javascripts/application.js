3a75615feb7bb7b2a6fd9a30224bf0007a928e43
Andrej Petričko <32256397+xPetricko@users.noreply.github.com>
2019-06-18 23:03:06 +0200

    as

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery-ui
+    
 //= require bootstrap
 //= require rails-ujs
 //= require activestorage
